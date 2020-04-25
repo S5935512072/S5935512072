f059c4557f2a96d846679a1677ed83011d1a4416
S5935512072 <nongfew240241.1@gmail.com>
2020-04-26 02:54:59 +0700

    Compatible with function and hosting

---
+++ dist/precache-manifest.f75f49661e978a235ca7822b73055ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3b039816bbb23657a33f0b2f90ed3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c20157d7748d7e21edee",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "175cf42ce3b52f13e79a",
+    "url": "/static/js/2.9c393704.chunk.js"
+  },
+  {
+    "revision": "bd6b762390574ac8c479bbcc74f7e273",
+    "url": "/static/js/2.9c393704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c20157d7748d7e21edee",
+    "url": "/static/js/main.f9371adc.chunk.js"
+  },
+  {
+    "revision": "8ad5000b25673a8cca0f",
+    "url": "/static/js/runtime-main.9cf060ca.js"
+  }
+]);
